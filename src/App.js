167a53f7a016f09385b5f27dce893774b466a002
ABHISHEK TIWARI <tiwariabhishek27611@gmail.com>
2021-10-01 15:31:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import TabComponent from "./TabComponent";
+export default function App() {
+  return (
+    <div className="App">
+      <TabComponent />
+    </div>
+  );
+}
